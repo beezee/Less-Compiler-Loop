d5c59e083c49aec00900433df194eeed3a01f442
beezee <brian.zeligson@gmail.com>
2012-01-31 18:24:35 -0500

    fixed loop

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 var less = require('less');
+var fs = require('fs');
 
 var parser = new(less.Parser);
-for (var i=1; i < 3; i++)
-{
-    parser.parse('@import "config' + i + '.less"; @import "style.less";', function (err, tree) {
-        if (err) { return console.error(err) }
-        console.log(tree.toCSS());
-    });   
+function compileEach(i) {
+        parser.parse('@import "config' + i + '.less"; @import "style.less";', function (err, tree) {
+            if (err) { return console.error(err) }
+            fs.writeFile("style"+ i +".css", tree.toCSS(), function(err) {
+                if(err) {
+                    console.log(err);
+                } else {
+                    console.log(i);
+                    if (i < 2) compileEach(i+1);
+                }
+            }); 
+        });
 }
+
+compileEach(1);
+
